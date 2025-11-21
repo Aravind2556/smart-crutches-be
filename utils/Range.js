d7fee5191fcd7cd0a20f9dae8fb798a95144f866
Aravind2556 <aravintharavinth193@gmail.com>
2025-11-21 18:04:55 +0530

    updated remove baterypercentage range

--- utils/Range.js
+++ utils/Range.js
@@ -1,10 +1,9 @@
 const url = "https://api.thingspeak.com/channels/3173942/feeds.json?api_key=EFGSQBE4SVYAAKBW&results"
  const urlTwo = "https://api.thingspeak.com/channels/3174087/feeds.json?api_key=30ZY359FM8STMZLO&results"
  const temperatureRange = 50
- const batteryPercentageRange = 50
- const spo2Range = 95
- const minHeartRateRange = 60
- const maxHeartRateRange = 120
+ const spo2Range = 93
+ const minHeartRateRange = 50
+ const maxHeartRateRange = 100
 
  module.exports = {
     url ,
